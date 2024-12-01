8574be004841704c01a75c870d24ad4e1fca6deb
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2024-12-01 22:48:31 +0000

    chore: add eslint & prettier formatting

--- frontend/components/UI/Popover.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-    <div class="rounded bg-secondary border border-tertiary p-2">
-        <slot />
-    </div>
-</template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,17 +1,15 @@
-import type { NuxtPage } from 'nuxt/schema'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
 
   runtimeConfig: {
     api: {
-      apiUrl: process.env.API_URL || "http://localhost:8000/",
+      apiUrl: process.env.API_URL || 'http://localhost:8000/'
     }
   },
 
-  compatibilityDate: "2024-12-01"
+  compatibilityDate: '2024-12-01'
 })
