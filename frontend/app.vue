da41a597601ee5ef8b9a5e8c239b2bd1ecbd8967
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2024-12-06 22:58:30 +0000

    feat(auth): setup components for register user

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,8 +4,8 @@ const token = useCookie('otterly_token')
 
 <template>
   <div class="w-screen h-screen flex">
-    <NuxtLayout v-if="token" name="sidebar" class="hidden md:block" />
-    <NuxtLayout v-if="token" name="navbar" />
+    <!-- <NuxtLayout v-if="token" name="sidebar" class="hidden md:block" />
+    <NuxtLayout v-if="token" name="navbar" /> -->
     <NuxtPage />
   </div>
 </template>
