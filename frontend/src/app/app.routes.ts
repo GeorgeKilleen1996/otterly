9bd8f694135c63546b8eb97d5ab4e59552363359
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-08 23:39:13 +0000

    feat(auth): setup basic auth routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './core/auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './core/auth/pages/register-page/register-page.component';
+import { ForgottenPasswordPageComponent } from './core/auth/pages/forgotten-password-page/forgotten-password-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent },
+      { path: 'forgotten-password', component: ForgottenPasswordPageComponent}
+    ]
+  }
+];
