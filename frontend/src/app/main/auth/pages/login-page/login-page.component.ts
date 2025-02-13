7dbf6e2a845698639d3030a77d8e9990832de30c
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-13 22:13:44 +0000

    style(auth): create login form w/ icons

--- frontend/src/app/main/auth/pages/login-page/login-page.component.ts
+++ frontend/src/app/main/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideLogIn } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, RouterLink, NgIcon],
+  viewProviders: [provideIcons({ lucideLogIn })],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
