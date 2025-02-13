f40f15dde4f7936a05d7098f2f302084f99360a5
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-13 21:54:26 +0000

    feat(auth): add hide/unhide password

--- frontend/src/app/main/auth/components/login-form/login-form.component.ts
+++ frontend/src/app/main/auth/components/login-form/login-form.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideAtSign, lucideKeyRound } from '@ng-icons/lucide';
+import { lucideAtSign, lucideEye, lucideEyeOff, lucideKeyRound } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule, NgIcon],
-  viewProviders: [provideIcons({ lucideKeyRound, lucideAtSign })],
+  viewProviders: [provideIcons({ lucideKeyRound, lucideAtSign, lucideEye, lucideEyeOff })],
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
-  loading = false;
+  
+  loading:boolean = false;
+  hiddenPassword:boolean = true;
+  
   
   loginForm = new FormGroup({
     email: new FormControl('', [
@@ -26,11 +29,15 @@ export class LoginFormComponent {
     ])
   });
 
-  login() {
+  protected login() {
     if (this.loginForm.valid) {
       this.loading = true;
       // Your login logic here
       // Don't forget to set loading back to false after API call
     }
+  }
+
+  protected togglePassword() {
+    this.hiddenPassword = !this.hiddenPassword;
   }
 }
