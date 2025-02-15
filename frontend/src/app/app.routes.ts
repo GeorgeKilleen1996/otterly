be79a6f6374b6166f123ae053426fe373c1f130e
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-15 19:34:42 +0000

    fix(auth): guards between logged in / out states

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,20 +4,26 @@ import { RegisterPageComponent } from './main/auth/pages/register-page/register-
 import { ForgottenPasswordPageComponent } from './main/auth/pages/forgotten-password-page/forgotten-password-page.component';
 import { authGuard } from './main/auth/guards/auth.guard';
 import { IndexPageComponent } from './main/pages/index-page/index-page.component';
+import { nonAuthGuard } from './main/auth/guards/non-auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     canActivate: [authGuard],
+    pathMatch: 'full',
     component: IndexPageComponent
   },
   {
     path: 'auth',
     children: [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
-      { path: 'login', component: LoginPageComponent },
-      { path: 'register', component: RegisterPageComponent },
-      { path: 'forgotten-password', component: ForgottenPasswordPageComponent}
+      { path: 'login', canActivate: [nonAuthGuard], component: LoginPageComponent },
+      { path: 'register', canActivate: [nonAuthGuard], component: RegisterPageComponent },
+      { path: 'forgotten-password', canActivate: [nonAuthGuard], component: ForgottenPasswordPageComponent}
     ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
--- frontend/src/app/main/auth/guards/non-auth.guard.ts
+++ frontend/src/app/main/auth/guards/non-auth.guard.ts
@@ -2,14 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const nonAuthGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (!authService.isLoggedIn()) {
-    router.navigate(['/auth/login'], { 
-      queryParams: { returnUrl: state.url }
-    });
+  if (authService.isLoggedIn()) {
+    router.navigate(['/']);
     return false;
   }
   return true;
