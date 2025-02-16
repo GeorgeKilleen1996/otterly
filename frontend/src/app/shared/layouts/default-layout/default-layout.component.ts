63467640b9f08be8deacee57620a89913565f25e
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-16 15:11:54 +0000

    feat(main): create both nav components + add to layout

--- frontend/src/app/shared/layouts/default-layout/default-layout.component.ts
+++ frontend/src/app/shared/layouts/default-layout/default-layout.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopNavbarComponent } from '../../components/top-navbar/top-navbar.component';
+import { MainNavbarComponent } from '../../components/main-navbar/main-navbar.component';
 
 @Component({
   selector: 'app-default-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TopNavbarComponent, MainNavbarComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss'
 })
