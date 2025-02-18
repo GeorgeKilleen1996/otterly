864810d8d6f5e3ac728f29ed754b33af688767fc
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-18 00:38:09 +0000

    feat(top-nav): add more buttons for inbox

--- frontend/src/app/shared/components/top-navbar/top-navbar.component.ts
+++ frontend/src/app/shared/components/top-navbar/top-navbar.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideBell, lucideCalendar, lucideCommand, lucideMoon, lucideSearch, lucideSun } from '@ng-icons/lucide';
+import { lucideBell, lucideCalendar, lucideCommand, lucideMail, lucideMoon, lucideSearch, lucideSun } from '@ng-icons/lucide';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @Component({
   selector: 'app-top-navbar',
   standalone: true,
   imports: [CommonModule, NgIcon, UserProfileComponent],
-  viewProviders: [provideIcons({ lucideBell, lucideMoon, lucideSun, lucideCalendar, lucideSearch, lucideCommand })],
+  viewProviders: [provideIcons({ lucideBell, lucideMoon, lucideSun, lucideCalendar, lucideSearch, lucideCommand, lucideMail })],
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.scss'
 })
--- frontend/src/app/shared/components/top-navbar/user-profile/user-profile.component.ts
+++ frontend/src/app/shared/components/top-navbar/user-profile/user-profile.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { UserAvatarComponent } from '../../../../UI/components/user-avatar/user-avatar.component';
 import { AuthService } from '../../../../main/auth/services/auth.service';
 import { BaseAccount } from '../../../../main/auth/interfaces/auth';
-import { Observable } from 'rxjs';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideChevronDown } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule, UserAvatarComponent],
+  imports: [CommonModule, UserAvatarComponent, NgIcon],
+  viewProviders: [provideIcons({ lucideChevronDown })],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
