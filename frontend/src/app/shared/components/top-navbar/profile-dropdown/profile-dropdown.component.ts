1866cd069ec5bbb572c323610d3bd449a18be55a
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-02-23 23:52:15 +0000

    style(ui): create profile navbar item dropdown list

--- frontend/src/app/shared/components/top-navbar/profile-dropdown/profile-dropdown.component.ts
+++ frontend/src/app/shared/components/top-navbar/profile-dropdown/profile-dropdown.component.ts
@@ -2,11 +2,15 @@ import { Component, Input } from '@angular/core';
 import { BaseAccount } from '../../../../main/auth/interfaces/auth';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../../../../UI/components/card/card.component';
+import { UserAvatarComponent } from '../../../../UI/components/user-avatar/user-avatar.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideCog, lucideCreditCard, lucideLayoutList, lucideLogOut, lucideUserRound, lucideUserRoundPlus, lucideUsersRound } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-profile-dropdown',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, UserAvatarComponent, NgIcon],
+  viewProviders: [provideIcons({ lucideUserRound, lucideCog, lucideCreditCard, lucideUsersRound, lucideUserRoundPlus, lucideLayoutList, lucideLogOut })],
   templateUrl: './profile-dropdown.component.html',
   styleUrl: './profile-dropdown.component.scss'
 })
