ac0570a7f5dd4fc7f710b099ba988c13601dd4f9
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-04-08 21:03:31 +0100

    refactor: replace angular with nuxt

--- frontend/src/app/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  baseApiUrl: 'http://localhost:8000',
-  baseWsUrl: 'ws://localhost:8000',
-  baseUrl: 'http://localhost:4200',
-}
--- frontend/src/app/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  baseApiUrl: 'http://TODO',
-  baseWsUrl: 'ws://TODO',
-  baseUrl: 'http://TODO',
-}
