9640e4eb72ae06969b6f27f163a8f4c5591cea62
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-04-13 16:01:59 +0100

    style(login): setup basic login page

---
+++ frontend/components/auth/login/formComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <UCard variant="subtle">
+    <!-- TODO: Providers e.g. Google etc. -->
+    <Placeholder class="h-32" />
+    <USeparator label="or" />
+    <!-- TODO: Default Login Form... Email & Password -->
+    <Placeholder class="h-32" />
+  </UCard>
+</template>
--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,3 +1,11 @@
 <template>
-  <div />
+  <div class="container mx-auto h-screen flex">
+    <!-- TODO: Animated otterly logo w/ feature icons floating around - set behind form on mobile -->
+    <div class="hidden lg:flex flex-1" />
+    <div
+      class="w-full md:max-w-md flex items-center justify-center md:px-10 px-4"
+    >
+      <AuthLoginFormComponent class="w-full" />
+    </div>
+  </div>
 </template>
