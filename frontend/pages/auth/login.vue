27954104503daf75ede450d89db3345031471962
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-04-14 22:52:18 +0100

    style(auth): create animated gradient svg mask (for Otterly Logo when I have it 🙄)

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container mx-auto h-screen flex">
     <!-- TODO: Animated otterly logo w/ feature icons floating around - set behind form on mobile -->
-    <div class="hidden lg:flex flex-1" />
+    <div class="hidden lg:flex flex-1 justify-center items-center">
+      <AuthLogoArt class="w-full max-w-[75%]" />
+    </div>
     <div
       class="w-full md:max-w-md flex items-center justify-center md:px-10 px-4"
     >
