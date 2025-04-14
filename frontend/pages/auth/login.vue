9c0a740d02f9622ec1d651b08ab460620e97e5bb
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-04-14 22:58:44 +0100

    style(UI): create twinkle background

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container mx-auto h-screen flex">
+  <div class="container mx-auto h-screen flex relative">
+    <UIBackgroundsTwinkleStar />
     <!-- TODO: Animated otterly logo w/ feature icons floating around - set behind form on mobile -->
     <div class="hidden lg:flex flex-1 justify-center items-center">
       <AuthLogoArt class="w-full max-w-[75%]" />
