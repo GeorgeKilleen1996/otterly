8fec056cd8fcc834607515ff4aa2a1c47c52bfa9
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-05-10 16:44:53 +0100

    style: setup base theme correctly

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-auto h-screen flex relative">
-    <div class="hidden lg:flex flex-1 justify-center items-center">
+    <div class="hidden lg:flex flex-1 justify-center items-center bg-info">
       <!-- TODO: Some sort of information section / slider?  -->
     </div>
     <div
