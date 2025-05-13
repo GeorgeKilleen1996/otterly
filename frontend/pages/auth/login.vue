19b3783b8940ac34ced8c95a0f63252b6167dc08
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2025-05-13 20:49:34 +0100

    style(auth): switch bg to primary

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="mx-auto h-screen flex relative">
     <div
-      class="hidden lg:flex flex-1 justify-center items-center bg-info relative"
+      class="hidden lg:flex flex-1 justify-center items-center bg-primary relative"
     >
       <img
         src="~/assets/img/otterly-logo.png"
         alt="Otterly Logo"
-        class="w-1/2 h-auto absolute opacity-20 -rotate-20 -bottom-[10%] -left-[10%]"
+        class="w-1/2 h-auto absolute opacity-15 -rotate-20 -bottom-[10%] -left-[10%]"
       />
       <!-- TODO: Some sort of information section / slider?  -->
     </div>
